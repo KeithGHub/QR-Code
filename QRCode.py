b35fffe49cedf30c2f0225de19ec2348935d5a45
OguKeith123 <keithogu1@gmail.com>
2026-01-30 02:46:55 -0500

    Implemented more lines of code for QR Code functionality

--- QRCode.py
+++ QRCode.py
@@ -1,1 +1,12 @@
 import qrcode
+
+url = input("Enter the URL: ").strip()
+file_path = "D:\\MainUser\\VSCode Project Files\\2. Personal VSCode Projects\\3. Extra Projects\\qrcode.png"
+
+qr = qrcode.QRCODE()
+qr.add_data(url)
+
+img = qr.make_image()
+img.save(file_path)
+
+print("QR Code generated successfully!")
