32ecbcb37a8dd029c9e3292930d66ca63f4ae2ee
OguKeith123 <keithogu1@gmail.com>
2026-01-30 03:12:58 -0500

    Fixed API usage and syntax errors in QR code generator

--- QRCode.py
+++ QRCode.py
@@ -1,10 +1,11 @@
 import qrcode
 
 url = input("Enter the URL: ").strip()
-file_path = "D:\\MainUser\\VSCode Project Files\\2. Personal VSCode Projects\\3. Extra Projects\\qrcode.png"
+file_path = (ex: "C:\\Users\\HP\\Desktop\\qrcode.png")
 
-qr = qrcode.QRCODE()
+qr = qrcode.QRCode()
 qr.add_data(url)
+qr.make(fit=True)
 
 img = qr.make_image()
 img.save(file_path)
